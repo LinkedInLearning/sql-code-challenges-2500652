df67c9b605523f1f9920f4f925bd9829226548fc
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-23 16:08:35 +0000

    notes

--- notes.sql
+++ notes.sql
@@ -3,9 +3,9 @@
 -- with SQLite in Codespaces.
 -- These two statements operate on the 
 -- restaurant.db SQLite database.
+
 SELECT *
 FROM Dishes;
-select FirstName
-from Customers
+
 SELECT *
 FROM Customers;
