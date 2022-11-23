d2d469a61b6d795c338d54a00bcb649169060e2c
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-23 08:47:47 +0100

    Add files via upload

---
+++ 02_library/challenges/02_02_addbook_ch.sql
@@ -0,0 +1,11 @@
+-- Add books to the library database.
+
+-- Title: Dracula
+-- Author: Bram Stoker
+-- Published: 1897
+-- ID Number: 4819277482
+
+-- Title: Gulliver’s Travels into Several Remote Nations of the World
+-- Author: Jonathan Swift
+-- Published: 1729
+-- ID Number: 4899254401
