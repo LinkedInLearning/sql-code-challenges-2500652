9a6a62caef5e4c316f6e5398db771c4fa54e504e
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:31:57 +0000

    mkmk

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,8 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+-- Erstelle zwei Buchstatistiken
+
+-- Die erste soll zeigen wie viele Bücher pro Jahr veröffentlicht wurden. 
+-- Die zweite soll zeigen, welche fünf Bücher am meisten ausgeliehen worden sind.
