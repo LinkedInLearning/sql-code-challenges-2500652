5d662a9c4d44a6ee8eff4d68eb56ca2f5c1f1d00
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:31:02 +0000

    mom

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,5 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+-- Erstelle eine Liste von Mitgliedern, 
+-- die am wenigsten/seltensten Bücher ausgeliehen haben.
