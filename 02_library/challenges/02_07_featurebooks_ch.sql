7d833eb60fa545b7e335e683ad163cd1b0063c44
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:31:33 +0000

    mjmj

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,7 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+
+-- Ertstelle eine Liste von Büchern, die von 1890 bis 1899 
+-- veröffentlicht wurden und im Moment ausleihbar sind.
