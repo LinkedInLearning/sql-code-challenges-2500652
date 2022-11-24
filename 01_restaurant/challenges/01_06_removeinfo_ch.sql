8db5d41edef8346ba0afbedaab9f19a99c086358
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:23:50 +0000

    sm

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,8 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+-- Entferne eine ungültige Reservierung aus der Datenbank.
+
+-- Ein Kunde namens Norby hat uns informiert, dass er seine Reservierung
+-- stornieren möchte. Heute ist der 24.Juli 2022.
