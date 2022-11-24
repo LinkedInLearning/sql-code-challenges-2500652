72169211eedf23e7cfcc00a66be54f58a178e095
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:28:04 +0000

    me

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,6 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+
+-- Finde heraus wie viele Kopien von 
+-- „Dracula“ im Moment verfügbar sind.
