72a551e38d4c7f740829afb0217aee3611c30c61
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:25:21 +0000

    amam

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,11 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+
+-- Kreiere eine Reservierung für einen Kunden, 
+-- dessen Daten schon in unserer Datenbank sein könnten oder nicht.
+
+-- Die Reservierung ist für:
+-- Sam McAdams (smac@kinetecoinc.com), 5 Personen
+-- August 12, 2022 at 6PM (18:00)
