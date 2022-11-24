0895234a132247539d0aed3993fbc652af5e9e8e
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:30:32 +0000

    dms

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,13 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+
+
+-- Pflege die Rückgabe der folgenden Bücher in die Datenbank hinein.
+-- Nutze dafür die Barcodes.
+-- Rückgabedatum ist der 5. Juli 2022.
+
+-- Buch 1: 6435968624
+-- Buch 2: 5677520613
+-- Buch 3: 8730298424
