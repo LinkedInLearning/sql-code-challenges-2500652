3bf397699d8f9fffe1c9c891fa52f17369ffff9d
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:24:17 +0000

    mmaa

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,7 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+
+-- Registriere einen Kunden für unsere Jubiläumsfeier.
+-- Der Kunde wird mit drei Freunden an der Feier teilnehmen.
