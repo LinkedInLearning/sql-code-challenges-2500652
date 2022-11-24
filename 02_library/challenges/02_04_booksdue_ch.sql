1a0a8cffd09b7a770f7c3bd245baac3dcd3310ef
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-24 17:30:00 +0000

    mmam

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,8 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+
+-- Erstelle eine Liste von Büchern, dessen Ausleihfrist am 13. Juli 2022 endet.
+-- Diese Liste soll auch den Namen des Ausleihers,
+-- sowie dessen Email-Adresse enthalten.
