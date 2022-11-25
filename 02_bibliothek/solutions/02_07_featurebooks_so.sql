bd8281600111e480a3522a8b94a1fc69817da13c
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 18:08:32 +0000

    update

--- 02_bibliothek/solutions/02_07_featurebooks_so.sql
+++ 02_bibliothek/solutions/02_07_featurebooks_so.sql
@@ -3,6 +3,7 @@
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
 
+/*
 SELECT Title, Barcode
 FROM Books
 WHERE Published BETWEEN 1890 AND 1899
@@ -11,3 +12,13 @@ AND (BookID NOT IN
   FROM Loans 
   WHERE ReturnedDate IS NULL)) 
 ORDER BY Title;
+*/
+
+SELECT Titel, Barcode
+FROM Buecher
+WHERE Erscheinungsdatum BETWEEN 1890 AND 1899
+AND (BuchID NOT IN 
+  (SELECT BuchID 
+  FROM Ausleihung 
+  WHERE RueckgabeDatum IS NULL)) 
+ORDER BY Titel;
