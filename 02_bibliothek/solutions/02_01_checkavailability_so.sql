bba81934d925c76dc7b36d6dda5cfdbbd021a911
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 17:51:48 +0000

    update

--- 02_bibliothek/solutions/02_01_checkavailability_so.sql
+++ 02_bibliothek/solutions/02_01_checkavailability_so.sql
@@ -1,6 +1,6 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
-
+/*
 SELECT COUNT(Books.Title) 
 FROM Books 
 WHERE Books.Title = 'Dracula';
@@ -21,3 +21,25 @@ SELECT
     WHERE Books.Title = 'Dracula' 
     AND Loans.ReturnedDate IS NULL)
   AS AvailableCopies;
+  */
+
+SELECT COUNT(Buecher.Titel) 
+FROM Buecher 
+WHERE Buecher.Titel = 'Dracula';
+
+SELECT COUNT(Buecher.Titel)
+FROM Ausleihung 
+JOIN Buecher ON Ausleihung.BuchID = Buecher.BuchID 
+WHERE Buecher.Titel = 'Dracula' 
+  AND Ausleihung.RueckgabeDatum IS NULL;
+
+SELECT
+  (SELECT COUNT(Buecher.Titel) 
+    FROM Buecher 
+    WHERE Buecher.Titel = 'Dracula') - 
+  (SELECT COUNT(Buecher.Titel)
+    FROM Buecher 
+    JOIN Ausleihung ON Buecher.BuchID = Ausleihung.BuchID 
+    WHERE Buecher.Titel = 'Dracula' 
+    AND Ausleihung.RueckgabeDatum IS NULL)
+  AS VerfuegbareKopien;
