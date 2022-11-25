0c6937c598d6757fa9efbdf18e58a923130a6edd
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 12:19:31 +0000

    max

--- 01_restaurant/solutions/01_03_menus_so.sql
+++ 01_restaurant/solutions/01_03_menus_so.sql
@@ -4,7 +4,7 @@
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
 
-SELECT Type, Name, Price, Description 
+/*SELECT Type, Name, Price, Description 
 FROM Dishes 
 ORDER BY Price;
 
@@ -16,4 +16,23 @@ ORDER BY Type;
 SELECT Type, Name, Price, Description 
 FROM Dishes 
 WHERE Type != 'Beverage'
-ORDER BY Type;
+ORDER BY Type; */
+
+
+SELECT Typ, Name, Preis, Beschreibung 
+FROM Speise 
+ORDER BY Preis;
+
+SELECT Typ, Name, Preis, Beschreibung 
+FROM Speise 
+WHERE Typ = 'Vorspeise' OR Typ = 'Getränk' 
+ORDER BY Typ;
+
+SELECT Typ, Name, Preis, Beschreibung 
+FROM Speise 
+WHERE Typ != 'Getränk'
+ORDER BY Typ;
+
+
+
+
