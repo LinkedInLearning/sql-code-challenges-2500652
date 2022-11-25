90280e1cccbab0bbbca7247d9c7292172fa29759
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 18:07:04 +0000

    update

--- 02_bibliothek/solutions/02_06_visitreminder_so.sql
+++ 02_bibliothek/solutions/02_06_visitreminder_so.sql
@@ -1,9 +1,18 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
 
+/*
 SELECT COUNT(Loans.LoanID) AS LoanCount, Patrons.FirstName, Patrons.Email
 FROM Loans
 JOIN Patrons ON Loans.PatronID=Patrons.PatronID
 GROUP BY Loans.PatronID
 ORDER BY LoanCount ASC
+LIMIT 15;
+*/
+
+SELECT COUNT(Ausleihung.AusleihID) AS AnzahlAusleihung, Mitglieder.Vorname, Mitglieder.Email
+FROM Ausleihung
+JOIN Mitglieder ON Ausleihung.MitgliedsID=Mitglieder.MitgliedsID
+GROUP BY Ausleihung.MitgliedsID
+ORDER BY AnzahlAusleihung ASC
 LIMIT 15;
