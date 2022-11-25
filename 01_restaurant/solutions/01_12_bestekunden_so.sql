76a1a97e156c451de0986aae4686995953051d27
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 13:34:11 +0000

    update

--- 01_restaurant/solutions/01_12_bestekunden_so.sql
+++ 01_restaurant/solutions/01_12_bestekunden_so.sql
@@ -2,9 +2,17 @@
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
 
-SELECT COUNT(Orders.orderID) as OrderCount, Customers.FirstName, Customers.LastName, Customers.Email
+/*SELECT COUNT(Orders.orderID) as OrderCount, Customers.FirstName, Customers.LastName, Customers.Email
 FROM Orders
 JOIN Customers ON Orders.CustomerID = Customers.CustomerID
 GROUP BY Orders.CustomerID
 ORDER BY OrderCount DESC
+LIMIT 15; */
+
+
+SELECT COUNT(Bestellungen.BestellungsID) as AnzahlBestellungen, Kunden.Vorname, Kunden.Nachname, Kunden.Email
+FROM Bestellungen
+JOIN Kunden ON Bestellungen.KundenID = Kunden.KundenID
+GROUP BY Bestellungen.KundenID
+ORDER BY AnzahlBestellungen DESC
 LIMIT 15;
