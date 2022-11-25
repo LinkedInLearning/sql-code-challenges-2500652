b76ac50125996b8ce29d8447a0884f6944ff5538
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 17:58:18 +0000

    update

--- 02_bibliothek/solutions/02_03_checkout_so.sql
+++ 02_bibliothek/solutions/02_03_checkout_so.sql
@@ -4,6 +4,7 @@
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
 
+/*
 INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
 VALUES (
   (SELECT BookID 
@@ -32,4 +33,35 @@ JOIN Books ON Loans.BookID = Books.BookID
 WHERE PatronID = (
   SELECT PatronID 
   FROM Patrons 
+  WHERE Email = 'jvaan@wisdompets.com');
+  */
+
+  INSERT INTO Ausleihung (BuchID, MitgliedsID, AusleihDatum, Rueckgabefrist)
+VALUES (
+  (SELECT BuchID 
+    FROM Buecher 
+    WHERE Barcode = '2855934983'), 
+  (SELECT MitgliedsID 
+    FROM Mitglieder 
+    WHERE Email = 'jvaan@wisdompets.com'),
+  '2022-08-25',
+  '2022-09-08');
+
+INSERT INTO Ausleihung (BuchID, MitgliedsID, AusleihDatum, Rueckgabefrist)
+VALUES (
+  (SELECT BuchID 
+    FROM Buecher 
+    WHERE Barcode = '4043822646'), 
+  (SELECT MitgliedsID 
+    FROM Mitglieder 
+    WHERE Email = 'jvaan@wisdompets.com'), 
+  '2022-08-25', 
+  '2022-09-08');
+
+SELECT * 
+FROM Ausleihung 
+JOIN Buecher ON Ausleihung.BuchID = Buecher.BuchID 
+WHERE MitgliedsID = (
+  SELECT MitgliedsID 
+  FROM Mitglieder 
   WHERE Email = 'jvaan@wisdompets.com');
--- 02_bibliothek/solutions/02_04_booksdue_so.sql
+++ 02_bibliothek/solutions/02_04_booksdue_so.sql
@@ -3,9 +3,18 @@
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
 
+/*
 SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
 FROM Loans
 JOIN Books ON Loans.BookID = Books.BookID
 JOIN Patrons ON Loans.PatronID = Patrons.PatronID
 WHERE Loans.DueDate = '2022-07-13' 
 AND Loans.ReturnedDate IS NULL;
+*/
+
+SELECT Ausleihung.Rueckgabefrist, Buecher.Titel, Mitglieder.Vorname, Mitglieder.Email
+FROM Ausleihung
+JOIN Buecher ON Ausleihung.BuchID = Buecher.BuchID
+JOIN Mitglieder ON Ausleihung.MitgliedsID = Mitglieder.MitgliedsID
+WHERE Ausleihung.Rueckgabefrist = '2022-07-13' 
+AND Ausleihung.RueckgabeDatum IS NULL;
