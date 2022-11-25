134a5cca64cb0381cc2e459fd2590036e7e7882c
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 13:20:30 +0000

    update

--- 01_restaurant/solutions/01_08_findereservierung_so.sql
+++ 01_restaurant/solutions/01_08_findereservierung_so.sql
@@ -6,9 +6,16 @@
 
 -- There are four people in the party. Today is June 14th.
 
-SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+/*SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
 FROM Reservations
 JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
 WHERE Customers.LastName LIKE 'St%'
 AND PartySize = 4
-ORDER BY Reservations.Date DESC;
+ORDER BY Reservations.Date DESC; */
+
+SELECT Kunden.Vorname, Kunden.Nachname, Reservierung.Datum, Reservierung.PartyGroesse
+FROM Reservierung
+JOIN Kunden ON Kunden.KundenID = Reservierung.KundenID
+WHERE Kunden.Nachname LIKE 'St%'
+AND PartyGroesse = 4
+ORDER BY Reservierung.Datum DESC;
