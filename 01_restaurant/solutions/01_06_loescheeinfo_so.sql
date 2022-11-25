26ad95ca465e035ff627f34ced06775e7af1f094
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 13:14:55 +0000

    update

--- 01_restaurant/solutions/01_06_loescheeinfo_so.sql
+++ 01_restaurant/solutions/01_06_loescheeinfo_so.sql
@@ -4,7 +4,7 @@
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
 
-SELECT * FROM Reservations
+/*SELECT * FROM Reservations
 JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
 WHERE Customers.FirstName = 'Norby'
 AND Reservations.Date > '2022-07-24';
@@ -12,4 +12,14 @@ AND Reservations.Date > '2022-07-24';
 DELETE FROM Reservations WHERE ReservationID = 2000;
 
 -- or:
-UPDATE Reservations SET Date = NULL WHERE ReservationID = 2000;
+UPDATE Reservations SET Date = NULL WHERE ReservationID = 2000; */
+
+SELECT * FROM Reservierung
+JOIN Kunden ON Reservierung.KundenID = Kunden.KundenID
+WHERE Kunden.Vorname = 'Norby'
+AND Reservierung.Datum > '2022-07-24';
+
+DELETE FROM Reservierung WHERE ReservierungsID = 2000;
+
+-- or:
+UPDATE Reservierung SET Datum = NULL WHERE ReservierungsID = 2000; 
