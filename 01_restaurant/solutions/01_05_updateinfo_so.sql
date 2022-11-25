e73073449265280c16b66db1f965790e6bf4a6d0
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 13:12:34 +0000

    Update

--- 01_restaurant/solutions/01_05_updateinfo_so.sql
+++ 01_restaurant/solutions/01_05_updateinfo_so.sql
@@ -3,7 +3,7 @@
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
 
-SELECT CustomerID, FirstName, LastName, Address 
+/*SELECT CustomerID, FirstName, LastName, Address 
 FROM Customers 
 WHERE FirstName = 'Taylor' 
 AND LastName = 'Jenkins';
@@ -16,4 +16,20 @@ WHERE CustomerID = 26;
 
 SELECT * 
 FROM Customers 
-WHERE CustomerID = 26;
+WHERE CustomerID = 26; */
+
+
+SELECT KundenID, Vorname, Nachname, Adresse 
+FROM Kunden 
+WHERE Vorname = 'Taylor' 
+AND Nachname = 'Jenkins';
+
+UPDATE Kunden 
+SET Adresse = '74 Pine St.', 
+    Stadt = 'New York', 
+    State = 'NY' 
+WHERE KundenID = 26;
+
+SELECT * 
+FROM Kunden 
+WHERE KundenID = 26;
