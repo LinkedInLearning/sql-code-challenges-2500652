66c5099d649820ebe3a1df071b1e744eaa04ea40
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 13:23:41 +0000

    update

--- 01_restaurant/solutions/01_09_kreierereservierung_so.sql
+++ 01_restaurant/solutions/01_09_kreierereservierung_so.sql
@@ -5,7 +5,7 @@
 -- Sam McAdams (smac@kinetecoinc.com, (555) 555-1232)
 -- for 5 people on August 12, 2022 at 6PM (18:00)
 
-SELECT * 
+/*SELECT * 
 FROM Customers 
 WHERE Email = 'smac@kinetecoinc.com';
 
@@ -24,4 +24,25 @@ INSERT INTO Reservations
 SELECT Customers.FirstName, Customers.LastName, Customers.Email, Reservations.Date, Reservations.PartySize 
 FROM Customers 
 JOIN Reservations ON Customers.CustomerID = Reservations.CustomerID 
-WHERE Customers.Email = 'smac@kinetecoinc.com';
+WHERE Customers.Email = 'smac@kinetecoinc.com'; */
+
+SELECT * 
+FROM Kunden 
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Kunden 
+(Vorname, Nachname, Email, Telefon) VALUES 
+('Sam', 'McAdams', 'smac@kinetecoinc.com', '(555) 555-1232');
+
+SELECT * 
+FROM Kunden 
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Reservierung 
+(KundenID, Datum, PartyGroesse) VALUES 
+(102, '2022-08-12 18:00:00', 5);
+
+SELECT Kunden.Vorname, Kunden.Nachname, Kunden.Email, Reservierung.Datum, Reservierung.PartyGroesse 
+FROM Kunden 
+JOIN Reservierung ON Kunden.KundenID = Reservierung.KundenID 
+WHERE Kunden.Email = 'smac@kinetecoinc.com';
