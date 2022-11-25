f41b820360bc40602b54febba4b72e371e95683d
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-25 17:53:23 +0000

    update

--- 02_bibliothek/solutions/02_02_addbook_so.sql
+++ 02_bibliothek/solutions/02_02_addbook_so.sql
@@ -10,6 +10,7 @@
 -- Published: 1729
 -- ID Number: 4899254401
 
+/*
 INSERT INTO Books (Title, Author, Published, Barcode)
 VALUES
 ('Dracula', 'Bram Stoker', '1897', '4819277482'),
@@ -17,4 +18,14 @@ VALUES
 
 SELECT * FROM Books
 ORDER BY BookID DESC
+LIMIT 5;
+*/
+
+INSERT INTO Buecher (Titel, AutorIn, Erscheinungsdatum, Barcode)
+VALUES
+('Dracula', 'Bram Stoker', '1897', '4819277482'),
+('Gulliver''s Travels into Several Remote Nations of the World', 'Jonathan Swift', '1729', '4899254401');
+
+SELECT * FROM Buecher
+ORDER BY BuchID DESC
 LIMIT 5;
