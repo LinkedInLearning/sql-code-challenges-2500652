8aebcb2fec233cfa910a2e7690baff2de81b5e44
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-26 18:59:14 +0000

    update

--- 01_restaurant/solutions/01_11_lieblingsgerichtupdate_so.sql
+++ 01_restaurant/solutions/01_11_lieblingsgerichtupdate_so.sql
@@ -34,9 +34,10 @@ FROM Kunden
 WHERE Vorname = 'Cleo' 
   AND Nachname = 'Goldwater';
 
+/*
 UPDATE Kunden 
 SET LieblingsGericht = 9 
-  WHERE KundenID = 42;
+  WHERE KundenID = 42; MA don't like this, left out in the script*/
 
 UPDATE Kunden 
   SET LieblingsGericht = (SELECT SpeiseID FROM Speise WHERE Name = 'Quinoa Salmon Salad') 
