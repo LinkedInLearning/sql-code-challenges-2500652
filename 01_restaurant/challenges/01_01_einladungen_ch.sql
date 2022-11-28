35ffaf18398fd219a01eacdcc594be8039cc026f
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-28 10:30:21 +0000

    update

--- 01_restaurant/challenges/01_01_einladungen_ch.sql
+++ 01_restaurant/challenges/01_01_einladungen_ch.sql
@@ -1,13 +1,6 @@
--- Generate a list of customer information.
-
--- Show their first name, last name, and email address.
--- Sort the list of results by last name.
-
-/* 
-- Kreiere eine Liste mit Kunden-Informationen.
-- Diese Liste soll Vornamen, Nachnamen 
-  und die E-Mail-Adresse der Kundinnen/Kunden enthalten.
-- Sortiere die Liste alphabetisch nach den Nachnamen
-*/
+-- Kreieren Sie eine Liste mit Kunden-Informationen.
+-- Diese Liste soll Vornamen, Nachnamen 
+-- und die E-Mail-Adresse der Kundinnen/Kunden enthalten.
+-- Sortieren Sie die Liste alphabetisch nach den Nachnamen.
 
 
