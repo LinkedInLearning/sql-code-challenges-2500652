b6e7a958f530afdd5ee897f036b670c0ad2010e3
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-28 10:29:19 +0000

    update

--- notes.sql
+++ notes.sql
@@ -1,13 +1,11 @@
--- This file is intended for notes
--- and for demonstrating how to work
--- with SQLite in Codespaces.
+-- Diese Datei wird für Notizen
+-- und zum Demonstrieren der Arbeitsweise
+-- mit SQLite in Codespaces verwendet.
 
-/*
-DE
-*/
 
--- These two statements operate on the 
--- restaurant.db SQLite database.
+-- Diese zwei Abfragen operieren auf der 
+-- restaurant.db SQLite Datenbank.
+
 SELECT * FROM Speise;
 SELECT * FROM Kunden;
 
