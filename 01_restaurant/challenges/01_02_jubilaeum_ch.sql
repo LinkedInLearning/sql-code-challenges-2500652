c22ff73bde1d68cc9ac3281124e493cf9fe8138c
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-28 10:31:21 +0000

    update

--- 01_restaurant/challenges/01_02_jubilaeum_ch.sql
+++ 01_restaurant/challenges/01_02_jubilaeum_ch.sql
@@ -1,17 +1,7 @@
--- Create a table in the database to store customer
--- responses to our anniversary invitation.
+-- Erstellen Sie eine neue Tabelle namens „Jubilaeum“ um die Antworten
+-- der Kundinnen/Kunden auf unsere Einladung zu speichern.
 
--- Associate a customer's ID number with the number of people
--- they plan to bring in their party.
+-- Die ID einer Kundin sollte der Anzahl der Personen, 
+-- die sie mitbringen will zugeordnet sein.
 
--- Hint: SQLite offers the INTEGER and REAL datatypes
-
-/*
-- Erstelle eine neue Tabelle namens „Jubilaeum“ um die Antworten
-  der Kundinnen/Kunden auf unsere Einladung zu speichern.
-
-- Die ID einer Kundin sollte der Anzahl der Personen, 
-  die sie mitbringen will zugeordnet sein.
-
-- Tipp: SQLite hat zwei verschiedene Zahlen-Datentypen INTEGER und REAL.
-*/
+-- Tipp: SQLite hat zwei verschiedene Zahlen-Datentypen INTEGER und REAL.
