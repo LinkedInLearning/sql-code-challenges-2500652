1d7da1ff014637c57332bf165a9ae0c2e7c308e3
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-28 14:10:07 +0000

    update

--- 01_restaurant/solutions/01_01_einladungen_so.sql
+++ 01_restaurant/solutions/01_01_einladungen_so.sql
@@ -3,11 +3,6 @@
 -- und die E-Mail-Adresse der Kundinnen/Kunden enthalten.
 -- Sortieren Sie die Liste alphabetisch nach den Nachnamen.
 
-/*
-SELECT FirstName, LastName, Email 
-FROM Customers 
-ORDER BY LastName;*/
-
 SELECT Vorname, Nachname, Email 
 FROM Kunden 
 ORDER BY Nachname;
