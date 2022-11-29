fab1b6987b562987950a264679d8bc8aa1e5959a
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-29 09:28:49 +0000

    update

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -1,4 +1,4 @@
--- Updaten Sie die Kontak-Informationen eines Kunden.
+-- Aktualisieren Sie die Kontak-Informationen eines Kunden.
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
