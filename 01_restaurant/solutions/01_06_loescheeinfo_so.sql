119c14b406b2d7fe27bc64fa8e467407a6f3cc5c
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-29 11:58:03 +0000

    update

--- 01_restaurant/solutions/01_06_loescheeinfo_so.sql
+++ 01_restaurant/solutions/01_06_loescheeinfo_so.sql
@@ -3,17 +3,6 @@
 -- Ein Kunde namens Norby hat uns informiert, dass er seine Reservierung
 -- für Freitag stornieren moechte. Heute ist der 24.Juli 2022.
 
-/* 
-SELECT * FROM Reservations
-JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
-WHERE Customers.FirstName = 'Norby'
-AND Reservations.Date > '2022-07-24';
-
-DELETE FROM Reservations WHERE ReservationID = 2000;
-
--- or:
-UPDATE Reservations SET Date = NULL WHERE ReservationID = 2000; */
-
 SELECT * FROM Reservierung
 JOIN Kunden ON Reservierung.KundenID = Kunden.KundenID
 WHERE Kunden.Vorname = 'Norby'
