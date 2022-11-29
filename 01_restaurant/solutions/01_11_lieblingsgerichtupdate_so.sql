13b82405c3b6cca93a685efc98a0afa2b12fe549
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-29 14:43:29 +0000

    update

--- 01_restaurant/solutions/01_11_lieblingsgerichtupdate_so.sql
+++ 01_restaurant/solutions/01_11_lieblingsgerichtupdate_so.sql
@@ -3,29 +3,6 @@
 -- Tragen Sie Cleo Goldwaters Lieblingsgericht 
 -- als "Quinoa Salmon Salad" ein.
 
-/*
-SELECT DishID 
-FROM Dishes 
-WHERE Name = 'Quinoa Salmon Salad';
-
-SELECT * 
-FROM Customers 
-WHERE FirstName = 'Cleo' 
-  AND LastName = 'Goldwater';
-
-UPDATE Customers 
-SET FavoriteDish = 9 
-  WHERE CustomerID = 42;
-
-UPDATE Customers 
-  SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad') 
-  WHERE CustomerID = 42;
-
-SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name 
-FROM Customers 
-JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID;*/
-
-
 SELECT SpeiseID 
 FROM Speise 
 WHERE Name = 'Quinoa Salmon Salad';
