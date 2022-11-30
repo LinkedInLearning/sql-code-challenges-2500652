6d907ea1d27c611f5059c67f4cca63c7ad7f374d
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 08:15:07 +0000

    update

--- 02_bibliothek/solutions/02_01_buchverfuegbarkeit_so.sql
+++ 02_bibliothek/solutions/02_01_buchverfuegbarkeit_so.sql
@@ -1,28 +1,6 @@
 -- Finden Sie heraus wie viele Kopien von 
 -- „Dracula“ im Moment verfügbar sind.
 
-/*
-SELECT COUNT(Books.Title) 
-FROM Books 
-WHERE Books.Title = 'Dracula';
-
-SELECT COUNT(Books.Title)
-FROM Loans 
-JOIN Books ON Loans.BookID = Books.BookID 
-WHERE Books.Title = 'Dracula' 
-  AND Loans.ReturnedDate IS NULL;
-
-SELECT
-  (SELECT COUNT(Books.Title) 
-    FROM Books 
-    WHERE Books.Title = 'Dracula') - 
-  (SELECT COUNT(Books.Title)
-    FROM Books 
-    JOIN Loans ON Books.BookID = Loans.BookID 
-    WHERE Books.Title = 'Dracula' 
-    AND Loans.ReturnedDate IS NULL)
-  AS AvailableCopies;
-  */
 
 SELECT COUNT(Buecher.Titel) 
 FROM Buecher 
