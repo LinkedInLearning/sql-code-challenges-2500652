d5b27f2ad9b256cbe07d852df987d1b9d038799d
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 10:43:33 +0000

    update

--- 02_bibliothek/solutions/02_07_besonderebuecher_so.sql
+++ 02_bibliothek/solutions/02_07_besonderebuecher_so.sql
@@ -1,17 +1,6 @@
 -- Ertstellen Sie eine Liste von Büchern, die von 1890 bis 1899  
 -- veröffentlicht wurden und im Moment ausgeliehen werden können. 
 
-/*
-SELECT Title, Barcode
-FROM Books
-WHERE Published BETWEEN 1890 AND 1899
-AND (BookID NOT IN 
-  (SELECT BookID 
-  FROM Loans 
-  WHERE ReturnedDate IS NULL)) 
-ORDER BY Title;
-*/
-
 SELECT Titel, Barcode
 FROM Buecher
 WHERE Erscheinungsdatum BETWEEN 1890 AND 1899
