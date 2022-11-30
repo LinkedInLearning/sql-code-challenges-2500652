7b3b15b88f7657a6fdf93859fc76accdc0665349
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 08:37:04 +0000

    update

--- 02_bibliothek/solutions/02_02_buchhinzufuegen_so.sql
+++ 02_bibliothek/solutions/02_02_buchhinzufuegen_so.sql
@@ -1,15 +1,6 @@
 -- Fügen Sie folgende Bücher in die Datenbank hinzu
 -- und stellen diese zum Ausleihen bereit.
 
-INSERT INTO Books (Title, Author, Published, Barcode)
-VALUES
-('Dracula', 'Bram Stoker', '1897', '4819277482'),
-('Gulliver''s Travels into Several Remote Nations of the World', 'Jonathan Swift', '1729', '4899254401');
-
-SELECT * FROM Books
-ORDER BY BookID DESC
-LIMIT 5;
-*/
 
 INSERT INTO Buecher (Titel, AutorIn, Erscheinungsdatum, Barcode)
 VALUES
