a8f36d68d336aae238f75032f243a8701d9f02f0
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 10:56:26 +0000

    update

--- 02_bibliothek/solutions/02_08_buchstatistiken_so.sql
+++ 02_bibliothek/solutions/02_08_buchstatistiken_so.sql
@@ -3,20 +3,6 @@
 -- Die erste soll zeigen wie viele Bücher pro Jahr veröffentlicht wurden. 
 -- Die zweite soll zeigen, welche fünf Bücher am meisten ausgeliehen worden sind.
 
-/*
-SELECT Published, COUNT(DISTINCT(Title)) AS PubCount 
-FROM Books 
-GROUP BY Published
-ORDER BY PubCount DESC;
-
-SELECT COUNT(Loans.LoanID) AS LoanCount, Books.Title 
-FROM Loans 
-JOIN Books ON Loans.BookID=Books.BookID 
-GROUP BY Books.Title 
-ORDER BY LoanCount DESC 
-LIMIT 5;
-*/
-
 SELECT Erscheinungsdatum, COUNT(DISTINCT(Titel)) AS AnzahlErscheinungen 
 FROM Buecher 
 GROUP BY Erscheinungsdatum
