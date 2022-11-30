48a57a0dc6377a6605df327593aeb8c41d2f6eee
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 08:35:24 +0000

    update

--- 02_bibliothek/solutions/02_02_buchhinzufuegen_so.sql
+++ 02_bibliothek/solutions/02_02_buchhinzufuegen_so.sql
@@ -1,18 +1,6 @@
 -- Fügen Sie folgende Bücher in die Datenbank hinzu
 -- und stellen diese zum Ausleihen bereit.
 
-/*
-- Titel: Dracula  
-- Autor: Bram Stoker 
-- Jahr: 1897
-- Neue ID: 4819277482
-
-- Titel: Gulliver’s Travels into Several Remote Nations of the World
-- Autor: Jonathan Swift
-- Jahr: 1729
-- Neue ID: 4899254401 */
-
-/*
 INSERT INTO Books (Title, Author, Published, Barcode)
 VALUES
 ('Dracula', 'Bram Stoker', '1897', '4819277482'),
