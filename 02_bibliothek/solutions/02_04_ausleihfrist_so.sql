56489164aaa0e8d14f150d13a0b794f6bbfc10b4
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 08:59:38 +0000

    update

--- 02_bibliothek/solutions/02_04_ausleihfrist_so.sql
+++ 02_bibliothek/solutions/02_04_ausleihfrist_so.sql
@@ -2,14 +2,6 @@
 -- Diese Liste soll auch den Namen der ausleihenden Person, 
 -- sowie deren Email-Adresse enthalten. 
 
-/*
-SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
-FROM Loans
-JOIN Books ON Loans.BookID = Books.BookID
-JOIN Patrons ON Loans.PatronID = Patrons.PatronID
-WHERE Loans.DueDate = '2022-07-13' 
-AND Loans.ReturnedDate IS NULL;
-*/
 
 SELECT Ausleihung.Rueckgabefrist, Buecher.Titel, Mitglieder.Vorname, Mitglieder.Email
 FROM Ausleihung
