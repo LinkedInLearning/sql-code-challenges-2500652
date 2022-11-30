449d9da61c0204c6bdc637d5962531fe3ec7b6fc
Makbule Asir <114994555+MakbuleAsir@users.noreply.github.com>
2022-11-30 10:39:10 +0000

    update

--- 02_bibliothek/solutions/02_06_seltenebesucher_so.sql
+++ 02_bibliothek/solutions/02_06_seltenebesucher_so.sql
@@ -1,14 +1,6 @@
 -- Erstellen Sie eine Liste von Mitgliedern,  
 -- die am seltensten Bücher ausgeliehen haben. 
 
-/*
-SELECT COUNT(Loans.LoanID) AS LoanCount, Patrons.FirstName, Patrons.Email
-FROM Loans
-JOIN Patrons ON Loans.PatronID=Patrons.PatronID
-GROUP BY Loans.PatronID
-ORDER BY LoanCount ASC
-LIMIT 15;
-*/
 
 SELECT COUNT(Ausleihung.AusleihID) AS AnzahlAusleihung, Mitglieder.Vorname, Mitglieder.Email
 FROM Ausleihung
